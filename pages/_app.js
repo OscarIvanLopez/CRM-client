46f88582ce1d91e5ccb002e71ab016b203836b34
OscarIvanLopez <oscari.lopez@potros.itson.edu.mx>
2020-07-08 21:20:42 -0700

    commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+const MyApp = ({ Component, pageProps }) => {
+
+    console.log("Desde _app.js");
+  return (
+  <Component {...pageProps} />
+  )
+};
+
+export default MyApp;
--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 const Login = () => {
   return (
-    <Fragment>
+    <Fragment> 
       <Layout>
         <h1 className="text-center text-2xl text-white font-light">Login</h1>
 
