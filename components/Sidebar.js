31864de0aa47162d6060b622bf3743edbbd24879
Ivan Lopez <oscari.lopez@potros.itson.edu.mx>
2020-07-11 22:09:32 -0700

    commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+// commit
 const Sidebar = () => {
   //routing de next
   const router = useRouter();
