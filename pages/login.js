a9ab6653f2ef62a841b1ec15342c49c22540c6f5
OscarIvanLopez <oscari.lopez@potros.itson.edu.mx>
2020-08-10 01:01:06 -0700

    10/ago/20

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,25 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
-import { useFormik } from "formik";
+import { useFormik, replace } from "formik";
 import * as Yup from "yup";
+import { gql, useMutation } from "@apollo/client";
+
+const AUTENTICAR_USUARIO = gql`
+  mutation autenticarUsuario($input: AutenticarInput) {
+    autenticarUsuario(input: $input) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
+
+  //Mensaje state
+  const [mensaje, guardarMensaje] = useState(null);
+
+  //Mutation para crear usuarios
+  const [autenticarUsuario] = useMutation(AUTENTICAR_USUARIO);
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -15,76 +31,103 @@ const Login = () => {
         .required("El email no puede ir vacio"),
       password: Yup.string().required("El password es obligatorio"),
     }),
+    onSubmit: async (valores) => {
+      // console.log(valores);
+      const { email, password } = valores;
+      try {
+        const { data } = await autenticarUsuario({
+          variables: {
+            input: {
+              email,
+              password,
+            },
+          },
+        });
+        console.log(data);
+      } catch (error) {
+        guardarMensaje(error.message.replace("GraphQL error: ", ""));
+        console.log(error);
+      }
+    },
   });
 
+  const mostrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 my-3 w-full max-w-sm text-center mx-auto ">
+        <p>{mensaje}</p>
+      </div>
+    );
+  };
+
   return (
-    <Fragment>
-      <Layout>
-        <h1 className="text-center text-2xl text-white font-light">Login</h1>
+    <Layout>
+      <h1 className="text-center text-2xl text-white font-light">Login</h1>
 
-        <div className="flex justify-center mt-5">
-          <div className="w-full max-w-sm">
-            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="shadow appearance-none  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="email"
-                  placeholder="Email Usuario"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.email}
-                />
-              </div>
-
-              {formik.touched.email && formik.errors.email ? (
-                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-                  <p className="font-bold">Error</p>
-                  <p>{formik.errors.email}</p>
-                </div>
-              ) : null}
-
-              <div>
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="shadow appearance-none  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  placeholder="Password Usuario"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-                />
-              </div>
-              
-              {formik.touched.password && formik.errors.password ? (
-                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-                  <p className="font-bold">Error</p>
-                  <p>{formik.errors.password}</p>
-                </div>
-              ) : null}
-
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-sm">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
               <input
-                type="submit"
-                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
-                value="Iniciar Sesion"
+                type="email"
+                className="shadow appearance-none  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                placeholder="Email Usuario"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
               />
-            </form>
-          </div>
+            </div>
+
+            {formik.touched.email && formik.errors.email ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            ) : null}
+
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="shadow appearance-none  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                placeholder="Password Usuario"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+            </div>
+
+            {formik.touched.password && formik.errors.password ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.password}</p>
+              </div>
+            ) : null}
+
+            <input
+              type="submit"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+              value="Iniciar Sesion"
+            />
+          </form>
         </div>
-      </Layout>
-    </Fragment>
+      </div>
+    </Layout>
   );
 };
 
